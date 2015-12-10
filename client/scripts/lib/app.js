5610ca90fc7bbe56b45b69be24ab0e6eb844c185
DAB0mB <emanor6@gmail.com>
2015-12-10 23:43:18 +0200

    Step 1.3: Create app.js file with the module initialization

---
+++ client/scripts/lib/app.js
@@ -0,0 +1,16 @@
+angular
+  .module('Whatsapp', [
+    'angular-meteor',
+    'ionic'
+  ]);
+
+if (Meteor.isCordova) {
+  angular.element(document).on('deviceready', onReady);
+}
+else {
+  angular.element(document).ready(onReady);
+}
+
+function onReady() {
+  angular.bootstrap(document, ['Whatsapp']);
+} 
