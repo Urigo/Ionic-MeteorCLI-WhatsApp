deecbe864f11edb180f1dcbb4716e12ab1ea340a
DAB0mB <emanor6@gmail.com>
2015-12-11 01:39:04 +0200

    Step 3.17: Add logic in the ChatCtrl for better experience

--- client/scripts/controllers/chat.controller.js
+++ client/scripts/controllers/chat.controller.js
@@ -2,11 +2,16 @@ angular
   .module('Whatsapp')
   .controller('ChatCtrl', ChatCtrl);
 
-function ChatCtrl ($scope, $reactive, $stateParams) {
+function ChatCtrl ($scope, $reactive, $stateParams, $ionicScrollDelegate, $timeout) {
   $reactive(this).attach($scope);
 
   let chatId = $stateParams.chatId;
+  let isIOS = ionic.Platform.isWebView() && ionic.Platform.isIOS();
+
   this.sendMessage = sendMessage;
+  this.inputUp = inputUp;
+  this.inputDown = inputDown;
+  this.closeKeyboard = closeKeyboard;
 
   this.helpers({
     messages() {
@@ -21,5 +26,27 @@ function ChatCtrl ($scope, $reactive, $stateParams) {
 
   function sendMessage () {
     // TODO: implement
+  }
+
+  function inputUp () {
+    if (isIOS) {
+      this.keyboardHeight = 216;
+    }
+
+    $timeout(function() {
+      $ionicScrollDelegate.$getByHandle('chatScroll').scrollBottom(true);
+    }, 300);
+  }
+
+  function inputDown () {
+    if (isIOS) {
+      this.keyboardHeight = 0;
+    }
+
+    $ionicScrollDelegate.$getByHandle('chatScroll').resize();
+  }
+
+  function closeKeyboard () {
+    // cordova.plugins.Keyboard.close();
   }
 }
