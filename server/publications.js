41a2e3accb8a15556fb7a5847bc5e86514e08d7d
DAB0mB <emanor6@gmail.com>
2015-12-11 16:10:37 +0200

    Step 6.5: Add publication to chats and messages data

--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,28 @@
 Meteor.publish('users', function () {
   return Meteor.users.find({}, { fields: { profile: 1 } });
 });
+
+Meteor.publishComposite('chats', function () {
+  if (! this.userId) return;
+
+  return {
+    find() {
+      return Chats.find({ userIds: this.userId });
+    },
+    children: [
+      {
+        find(chat) {
+          return Messages.find({ chatId: chat._id });
+        }
+      },
+      {
+        find(chat) {
+          let query = { _id: { $in: chat.userIds } };
+          let options = { fields: { profile: 1 } };
+
+          return Meteor.users.find(query, options);
+        }
+      }
+    ]
+  };
+});
