683dcf7306487dc9abde607c4439f05ed9ee169c
DAB0mB <emanor6@gmail.com>
2015-12-11 16:24:21 +0200

    Step 8.1: Add logic for sending image message

--- client/scripts/controllers/chat.controller.js
+++ client/scripts/controllers/chat.controller.js
@@ -2,7 +2,7 @@ angular
   .module('Whatsapp')
   .controller('ChatCtrl', ChatCtrl);
 
-function ChatCtrl ($scope, $reactive, $stateParams, $ionicScrollDelegate, $timeout) {
+function ChatCtrl ($scope, $reactive, $stateParams, $ionicScrollDelegate, $timeout, $ionicPopup, $log) {
   $reactive(this).attach($scope);
 
   let chatId = $stateParams.chatId;
@@ -12,6 +12,7 @@ function ChatCtrl ($scope, $reactive, $stateParams, $ionicScrollDelegate, $timeo
   this.inputUp = inputUp;
   this.inputDown = inputDown;
   this.closeKeyboard = closeKeyboard;
+  this.sendPicture = sendPicture;
 
   this.helpers({
     messages() {
@@ -28,6 +29,28 @@ function ChatCtrl ($scope, $reactive, $stateParams, $ionicScrollDelegate, $timeo
   });
 
   ////////////
+
+  function sendPicture () {
+    MeteorCameraUI.getPicture({}, (err, data) => {
+      if (err && err.error == 'cancel') return;
+      if (err) return handleError(err);
+
+      Meteor.call('newMessage', {
+        picture: data,
+        type: 'picture',
+        chatId: chatId
+      });
+    });
+  }
+
+  function handleError (err) {
+    $log.error('profile save error ', err);
+    $ionicPopup.alert({
+      title: err.reason || 'Save failed',
+      template: 'Please try again',
+      okType: 'button-positive button-clear'
+    });
+  }
 
   function sendMessage () {
     if (_.isEmpty(this.message)) return;
