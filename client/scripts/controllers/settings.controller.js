47f91ea8c0c8fc488f3295cfffd1778bb01c095d
DAB0mB <emanor6@gmail.com>
2015-12-11 15:33:13 +0200

    Step 4.18: Add settings controller with logout login

---
+++ client/scripts/controllers/settings.controller.js
@@ -0,0 +1,18 @@
+angular
+  .module('Whatsapp')
+  .controller('SettingsCtrl', SettingsCtrl);
+
+function SettingsCtrl($scope, $reactive, $state) {
+  $reactive(this).attach($scope);
+
+  this.logout = logout;
+
+  ////////////
+
+  function logout() {
+    Meteor.logout((err) => {
+      if (err) return;
+      $state.go('login');
+    });
+  }
+}
