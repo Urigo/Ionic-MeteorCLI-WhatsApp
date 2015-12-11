d52bb21871c7a371cfc5c2a19bc9b30136e72a80
DAB0mB <emanor6@gmail.com>
2015-12-11 15:44:57 +0200

    Step 5.5: Add NewChatCtrl controller

---
+++ client/scripts/controllers/new-chat.controller.js
@@ -0,0 +1,36 @@
+angular
+  .module('Whatsapp')
+  .controller('NewChatCtrl', NewChatCtrl);
+
+function NewChatCtrl($scope, $reactive, $state, NewChat) {
+  $reactive(this).attach($scope);
+
+  this.hideNewChatModal = hideNewChatModal;
+  this.newChat = newChat;
+
+  this.helpers({
+    users() {
+      return Meteor.users.find({ _id: { $ne: Meteor.userId() } });
+    }
+  });
+
+  ////////////
+
+  function hideNewChatModal() {
+    NewChat.hideModal();
+  }
+
+  function newChat(userId) {
+    let chat = Chats.findOne({ type: 'chat', userIds: { $all: [Meteor.userId(), userId] } });
+    if (chat) {
+      return goToChat(chat._id);
+    }
+
+    Meteor.call('newChat', userId, goToChat);
+  }
+
+  function goToChat(chatId) {
+    hideNewChatModal();
+    return $state.go('tab.chat', { chatId: chatId });
+  }
+}
