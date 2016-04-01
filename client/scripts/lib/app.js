8a1217eedbaa71c64ea8a97d5a0518c3362ab860
DAB0mB <emanor6@gmail.com>
2016-04-01 18:44:59 +0300

    Step 1.5: Load routes config

--- client/scripts/lib/app.js
+++ client/scripts/lib/app.js
@@ -5,9 +5,11 @@ import 'angular-sanitize';
 import 'angular-ui-router';
 import 'ionic-scripts';
 import Angular from 'angular';
+import Loader from 'angular-ecmascript/module-loader';
 import { Meteor } from 'meteor/meteor';
 
 // Modules
+import RoutesConfig from '../routes';
 
 const App = 'Whatsapp';
 
@@ -16,6 +18,9 @@ Angular.module(App, [
   'angular-meteor',
   'ionic'
 ]);
+
+new Loader(App)
+  .load(RoutesConfig);
 
 // Startup
 if (Meteor.isCordova) {
