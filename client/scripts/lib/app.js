022ddc4e5b47de782ae194025ff9e16309bbf353
DAB0mB <emanor6@gmail.com>
2016-04-01 18:21:27 +0300

    Step 1.2: Create main app file

---
+++ client/scripts/lib/app.js
@@ -0,0 +1,30 @@
+// Libs
+import 'angular-animate';
+import 'angular-meteor';
+import 'angular-sanitize';
+import 'angular-ui-router';
+import 'ionic-scripts';
+import Angular from 'angular';
+import { Meteor } from 'meteor/meteor';
+
+// Modules
+
+const App = 'Whatsapp';
+
+// App
+Angular.module(App, [
+  'angular-meteor',
+  'ionic'
+]);
+
+// Startup
+if (Meteor.isCordova) {
+  Angular.element(document).on('deviceready', onReady);
+}
+else {
+  Angular.element(document).ready(onReady);
+}
+
+function onReady() {
+  Angular.bootstrap(document, [App]);
+}
