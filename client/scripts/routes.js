751064efc9131caa79068d19cd75d50498bf7f3e
DAB0mB <emanor6@gmail.com>
2016-04-01 18:40:40 +0300

    Step 1.4: Add initial routes

---
+++ client/scripts/routes.js
@@ -0,0 +1,24 @@
+import { Config } from 'angular-ecmascript/module-helpers';
+
+export default class RoutesConfig extends Config {
+  configure() {
+    this.$stateProvider
+      .state('tab', {
+        url: '/tab',
+        abstract: true,
+        templateUrl: 'client/templates/tabs.html'
+      })
+      .state('tab.chats', {
+        url: '/chats',
+        views: {
+          'tab-chats': {
+            templateUrl: 'client/templates/chats.html'
+          }
+        }
+      });
+
+    this.$urlRouterProvider.otherwise('tab/chats');
+  }
+}
+
+RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
