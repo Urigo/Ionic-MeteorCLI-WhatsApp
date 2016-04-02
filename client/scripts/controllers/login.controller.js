52c04289311cd0a9ebc7c2474074b9f5a5ba331c
DAB0mB <emanor6@gmail.com>
2016-04-03 00:17:17 +0300

    Step 4.7: Add login controller

---
+++ client/scripts/controllers/login.controller.js
@@ -0,0 +1,45 @@
+import { _ } from 'meteor/underscore';
+import { Accounts } from 'meteor/accounts-base';
+import { Controller } from 'angular-ecmascript/module-helpers';
+
+export default class LoginCtrl extends Controller {
+  login() {
+    if (_.isEmpty(this.phone)) return;
+
+    const confirmPopup = this.$ionicPopup.confirm({
+      title: 'Number confirmation',
+      template: '<div>' + this.phone + '</div><div>Is your phone number above correct?</div>',
+      cssClass: 'text-center',
+      okText: 'Yes',
+      okType: 'button-positive button-clear',
+      cancelText: 'edit',
+      cancelType: 'button-dark button-clear'
+    });
+
+    confirmPopup.then((res) => {
+      if (!res) return;
+
+      this.$ionicLoading.show({
+        template: 'Sending verification code...'
+      });
+
+      Accounts.requestPhoneVerification(this.phone, (err) => {
+        this.$ionicLoading.hide();
+        if (err) return this.handleError(err);
+        this.$state.go('confirmation', { phone: this.phone });
+      });
+    });
+  }
+
+  handleError(err) {
+    this.$log.error('Login error ', err);
+
+    this.$ionicPopup.alert({
+      title: err.reason || 'Login failed',
+      template: 'Please try again',
+      okType: 'button-positive button-clear'
+    });
+  }
+}
+
+LoginCtrl.$inject = ['$state', '$ionicLoading', '$ionicPopup', '$log'];
