7536c7617c78cf1938ffab8c79090243023104a0
DAB0mB <emanor6@gmail.com>
2016-04-02 23:54:37 +0300

    Step 4.4: Create auth route states

--- client/scripts/routes.js
+++ client/scripts/routes.js
@@ -25,6 +25,21 @@ export default class RoutesConfig extends Config {
             controller: 'ChatCtrl as chat'
           }
         }
+      })
+      .state('login', {
+        url: '/login',
+        templateUrl: 'client/templates/login.html',
+        controller: 'LoginCtrl as logger'
+      })
+      .state('confirmation', {
+        url: '/confirmation/:phone',
+        templateUrl: 'client/templates/confirmation.html',
+        controller: 'ConfirmationCtrl as confirmation'
+      })
+      .state('profile', {
+        url: '/profile',
+        templateUrl: 'client/templates/profile.html',
+        controller: 'ProfileCtrl as profile'
       });
 
     this.$urlRouterProvider.otherwise('tab/chats');
