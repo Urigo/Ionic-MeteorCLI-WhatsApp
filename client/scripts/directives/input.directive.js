f9fd9ef698c275c80797abb9a0fa68d2d2474725
DAB0mB <emanor6@gmail.com>
2016-04-02 16:47:56 +0300

    Step 3.15: Add input directive

---
+++ client/scripts/directives/input.directive.js
@@ -0,0 +1,50 @@
+import { Directive } from '../entities';
+
+export default class input extends Directive {
+  constructor() {
+    super(...arguments);
+
+    this.restrict = 'E';
+
+    this.scope = {
+      'returnClose': '=',
+      'onReturn': '&',
+      'onFocus': '&',
+      'onBlur': '&'
+    };
+  }
+
+  link(scope, element) {
+    element.bind('focus', (e) => {
+      if (!scope.onFocus) return;
+
+      this.$timeout(() => {
+        scope.onFocus();
+      });
+    });
+
+    element.bind('blur', (e) => {
+      if (!scope.onBlur) return;
+
+      this.$timeout(() => {
+        scope.onBlur();
+      });
+    });
+
+    element.bind('keydown', (e) => {
+      if (e.which != 13) return;
+
+      if (scope.returnClose) {
+        element[0].blur();
+      }
+
+      if (scope.onReturn) {
+        this.$timeout(() => {
+          scope.onReturn();
+        });
+      }
+    });
+  }
+}
+
+input.$inject = ['$timeout'];
