9016f604e2a9f8178d0f71ac931931700422fc8e
DAB0mB <emanor6@gmail.com>
2016-04-02 23:50:40 +0300

    Step 4.4: Add SMS configuration

---
+++ server/sms.js
@@ -0,0 +1,7 @@
+import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
+
+if (Meteor.settings && Meteor.settings.ACCOUNTS_PHONE) {
+  Accounts._options.adminPhoneNumbers = Meteor.settings.ACCOUNTS_PHONE.ADMIN_NUMBERS;
+  Accounts._options.phoneVerificationMasterCode = Meteor.settings.ACCOUNTS_PHONE.MASTER_CODE;
+}
