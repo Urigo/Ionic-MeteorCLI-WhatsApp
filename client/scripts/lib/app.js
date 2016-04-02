961ee4f688f5f341840685345d5d7048bfc42f3b
DAB0mB <emanor6@gmail.com>
2016-04-02 16:18:32 +0300

    Step 3.4: Load chat controller

--- client/scripts/lib/app.js
+++ client/scripts/lib/app.js
@@ -10,6 +10,7 @@ import { Meteor } from 'meteor/meteor';
 
 // Modules
 import ChatsCtrl from '../controllers/chats.controller';
+import ChatCtrl from '../controllers/chat.controller';
 import CalendarFilter from '../filters/calendar.filter';
 import RoutesConfig from '../routes';
 
@@ -23,6 +24,7 @@ Angular.module(App, [
 
 new Loader(App)
   .load(ChatsCtrl)
+  .load(ChatCtrl)
   .load(CalendarFilter)
   .load(RoutesConfig);
 
