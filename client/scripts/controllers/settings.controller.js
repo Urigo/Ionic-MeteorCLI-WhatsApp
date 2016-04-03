18edd339640256f133fe5ef18b7c304f867560cd
DAB0mB <emanor6@gmail.com>
2016-04-03 23:55:04 +0300

    Step 4.24: Add settings controller

---
+++ client/scripts/controllers/settings.controller.js
@@ -0,0 +1,23 @@
+import { Meteor } from 'meteor/meteor';
+import { Controller } from 'angular-ecmascript/module-helpers';
+
+export default class SettingsCtrl extends Controller {
+  logout() {
+    Meteor.logout((err) => {
+      if (err) return this.handleError(err);
+      this.$state.go('login');
+    })
+  }
+
+  handleError (err) {
+    this.$log.error('Settings modification error', err);
+
+    this.$ionicPopup.alert({
+      title: err.reason || 'Settings modification failed',
+      template: 'Please try again',
+      okType: 'button-positive button-clear'
+    });
+  }
+}
+
+SettingsCtrl.$inject = ['$state', '$ionicPopup', '$log'];
