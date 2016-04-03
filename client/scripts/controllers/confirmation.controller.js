aa9e2ae696eacd09c425ab34baf95ad9e3ec1447
DAB0mB <emanor6@gmail.com>
2016-04-03 21:50:37 +0300

    Step 4.14: Create confirmation controller

---
+++ client/scripts/controllers/confirmation.controller.js
@@ -0,0 +1,32 @@
+import { _ } from 'meteor/underscore';
+import { Accounts } from 'meteor/accounts-base';
+import { Controller } from 'angular-ecmascript/module-helpers';
+
+export default class ConfirmationCtrl extends Controller {
+  constructor() {
+    super(...arguments);
+
+    this.phone = this.$state.params.phone;
+  }
+
+  confirm() {
+    if (_.isEmpty(this.code)) return;
+
+    Accounts.verifyPhone(this.phone, this.code, (err) => {
+      if (err) return this.handleError(err);
+      this.$state.go('profile');
+    });
+  }
+
+  handleError(err) {
+    this.$log.error('Confirmation error ', err);
+
+    this.$ionicPopup.alert({
+      title: err.reason || 'Confirmation failed',
+      template: 'Please try again',
+      okType: 'button-positive button-clear'
+    });
+  }
+}
+
+ConfirmationCtrl.$inject = ['$state', '$ionicPopup', '$log'];
