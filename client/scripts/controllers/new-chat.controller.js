ed26b968c704bb92b5426693433c191d79ccb0ad
DAB0mB <emanor6@gmail.com>
2016-04-04 00:27:03 +0300

    Step 5.6: Add new chat controller

---
+++ client/scripts/controllers/new-chat.controller.js
@@ -0,0 +1,51 @@
+import { Meteor } from 'meteor/meteor';
+import { Controller } from 'angular-ecmascript/module-helpers';
+import { Chats } from '../../../lib/collections';
+
+export default class NewChatCtrl extends Controller {
+  constructor() {
+    super(...arguments);
+
+    this.helpers({
+      users() {
+        return Meteor.users.find({ _id: { $ne: this.currentUserId } });
+      }
+    });
+  }
+
+  newChat(userId) {
+    let chat = Chats.findOne({ userIds: { $all: [this.currentUserId, userId] } });
+
+    if (chat) {
+      this.hideNewChatModal();
+      return this.goToChat(chat._id);
+    }
+
+    this.callMethod('newChat', userId, (err, chatId) => {
+      this.hideNewChatModal();
+      if (err) return this.handleError(err);
+      this.goToChat(chatId);
+    });
+  }
+
+  hideNewChatModal() {
+    this.NewChat.hideModal();
+  }
+
+  goToChat(chatId) {
+    this.$state.go('tab.chat', { chatId });
+  }
+
+  handleError(err) {
+    this.$log.error('New chat creation error ', err);
+
+    this.$ionicPopup.alert({
+      title: err.reason || 'New chat creation failed',
+      template: 'Please try again',
+      okType: 'button-positive button-clear'
+    });
+  }
+}
+
+NewChatCtrl.$name = 'NewChatCtrl';
+NewChatCtrl.$inject = ['$state', 'NewChat', '$ionicPopup', '$log'];
