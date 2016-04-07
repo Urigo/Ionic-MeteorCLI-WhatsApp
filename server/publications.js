0cd68f844d04cd616dfb0929231fc2c9517e6f83
DAB0mB <emanor6@gmail.com>
2016-04-08 00:26:21 +0300

    Step 6.5: Add chats publication

--- server/publications.js
+++ server/publications.js
@@ -1,5 +1,31 @@
 import { Meteor } from 'meteor/meteor';
+import { Chats, Messages } from '../lib/collections';
 
 Meteor.publish('users', function() {
   return Meteor.users.find({}, { fields: { profile: 1 } });
+});
+
+Meteor.publishComposite('chats', function() {
+  if (!this.userId) return;
+
+  return {
+    find() {
+      return Chats.find({ userIds: this.userId });
+    },
+    children: [
+      {
+        find(chat) {
+          return Messages.find({ chatId: chat._id });
+        }
+      },
+      {
+        find(chat) {
+          const query = { _id: { $in: chat.userIds } };
+          const options = { fields: { profile: 1 } };
+
+          return Meteor.users.find(query, options);
+        }
+      }
+    ]
+  };
 });
