e7a317c683956994f8b2e07871b72a3618300481
DAB0mB <emanor6@gmail.com>
2016-04-08 00:16:52 +0300

    Step 5.17: Create users with phone data stub

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -1,66 +1,27 @@
-import Moment from 'moment';
 import { Meteor } from 'meteor/meteor';
-import { Chats, Messages } from '../lib/collections';
+import { Accounts } from 'meteor/accounts-base';
 
 Meteor.startup(function() {
-  if (Chats.find().count() !== 0) return;
+  if (Meteor.users.find().count() != 0) return;
 
-  Messages.remove({});
-
-  const messages = [
-    {
-      text: 'You on your way?',
-      timestamp: Moment().subtract(1, 'hours').toDate()
-    },
-    {
-      text: 'Hey, it\'s me',
-      timestamp: Moment().subtract(2, 'hours').toDate()
-    },
-    {
-      text: 'I should buy a boat',
-      timestamp: Moment().subtract(1, 'days').toDate()
-    },
-    {
-      text: 'Look at my mukluks!',
-      timestamp: Moment().subtract(4, 'days').toDate()
-    },
-    {
-      text: 'This is wicked good ice cream.',
-      timestamp: Moment().subtract(2, 'weeks').toDate()
+  Accounts.createUserWithPhone({
+    phone: '+972501234567',
+    profile: {
+      name: 'My friend 1'
     }
-  ];
-
-  messages.forEach((m) => {
-    Messages.insert(m);
   });
 
-  const chats = [
-    {
-      name: 'Ethan Gonzalez',
-      picture: 'https://randomuser.me/api/portraits/thumb/men/1.jpg'
-    },
-    {
-      name: 'Bryan Wallace',
-      picture: 'https://randomuser.me/api/portraits/thumb/lego/1.jpg'
-    },
-    {
-      name: 'Avery Stewart',
-      picture: 'https://randomuser.me/api/portraits/thumb/women/1.jpg'
-    },
-    {
-      name: 'Katie Peterson',
-      picture: 'https://randomuser.me/api/portraits/thumb/women/2.jpg'
-    },
-    {
-      name: 'Ray Edwards',
-      picture: 'https://randomuser.me/api/portraits/thumb/men/2.jpg'
+  Accounts.createUserWithPhone({
+    phone: '+972501234568',
+    profile: {
+      name: 'My friend 2'
     }
-  ];
+  });
 
-  chats.forEach((chat) => {
-    const message = Messages.findOne({ chatId: { $exists: false } });
-    chat.lastMessage = message;
-    const chatId = Chats.insert(chat);
-    Messages.update(message._id, { $set: { chatId } });
+  Accounts.createUserWithPhone({
+    phone: '+972501234569',
+    profile: {
+      name: 'My friend 3'
+    }
   });
 });
