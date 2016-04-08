b4bc82fa4486a8add8a4b5103b5961462bd1da7a
DAB0mB <emanor6@gmail.com>
2016-04-08 18:11:54 +0300

    Step 5.14: Load chat picture filter

--- client/scripts/lib/app.js
+++ client/scripts/lib/app.js
@@ -21,6 +21,7 @@ import SettingsCtrl from '../controllers/settings.controller';
 import InputDirective from '../directives/input.directive';
 import CalendarFilter from '../filters/calendar.filter';
 import ChatNameFilter from '../filters/chat-name.filter';
+import ChatPictureFilter from '../filters/chat-picture.filter';
 import NewChatService from '../services/new-chat.service';
 import Routes from '../routes';
 
@@ -45,6 +46,7 @@ new Loader(App)
   .load(InputDirective)
   .load(CalendarFilter)
   .load(ChatNameFilter)
+  .load(ChatPictureFilter)
   .load(NewChatService)
   .load(Routes);
 
