2856a230a9079c4b378c36fc90be6a9b3dcd7414
DAB0mB <emanor6@gmail.com>
2016-04-10 23:43:12 +0300

    Step 4.27: Add auto-scroll to chat controller

--- client/scripts/controllers/chat.controller.js
+++ client/scripts/controllers/chat.controller.js
@@ -20,6 +20,8 @@ export default class ChatCtrl extends Controller {
         return Chats.findOne(this.chatId);
       }
     });
+
+    this.autoScroll();
   }
 
   sendMessage() {
@@ -54,6 +56,17 @@ export default class ChatCtrl extends Controller {
     if (this.isCordova) {
       cordova.plugins.Keyboard.close();
     }
+  }
+
+  autoScroll() {
+    let recentMessagesNum = this.messages.length;
+
+    this.autorun(() => {
+      const currMessagesNum = this.getCollectionReactively('messages').length;
+      const animate = recentMessagesNum != currMessagesNum;
+      recentMessagesNum = currMessagesNum;
+      this.scrollBottom(animate);
+    });
   }
 
   scrollBottom(animate) {
